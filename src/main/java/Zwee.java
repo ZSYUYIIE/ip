4d910f3cf05dea9e976df2a55f56fa0155896919
ZSYUYIIE <dr.dotiiee@gmail.com>
2026-01-20 19:04:53 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Zwee.java
@@ -0,0 +1,8 @@
+public class Zwee {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Zwee\n" +
+                "What can I do for you?\n\n" + 
+                "Bye. Hope to see you again soon!"
+            );
+    }
+}
