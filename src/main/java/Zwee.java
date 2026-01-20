660e5e616fabc3b6b23611a4cb4f1c832611934a
ZSYUYIIE <dr.dotiiee@gmail.com>
2026-01-20 19:43:54 +0800

    Level 1. Echo

--- src/main/java/Zwee.java
+++ src/main/java/Zwee.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Zwee {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Zwee\n" +
-                "What can I do for you?\n\n" + 
-                "Bye. Hope to see you again soon!"
+                "What can I do for you?\n"
             );
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                scanner.close();
+                break;
+            }
+            System.out.println(input);
+        }
     }
 }
