b0aa0a8ef0f0739fc2bd741c1b0ab329ca651204
zuyigac <dr.dotiiee@gmail.com>
2026-02-06 15:05:07 +0800

    Add javadoc for Ui

--- src/main/java/zwee/ui/Ui.java
+++ src/main/java/zwee/ui/Ui.java
@@ -4,34 +4,62 @@ import java.util.Scanner;
 import zwee.task.Task;
 import zwee.task.TaskList;
 
+/**
+ * Handles all interactions with the user.
+ * Responsible for reading input and displaying messages to the console.
+ */
 public class Ui {
 
     private static final String LINE = "________________________________________";
 
     private final Scanner scanner = new Scanner(System.in);
 
+    /**
+     * Displays the initial greeting message to the user.
+     */
     public void showWelcome() {
         System.out.println("Hello! I'm Zwee");
         System.out.println("What can I do for you?");
         showLine();
     }
-
+    
+    /**
+     * Displays the goodbye message to the user.
+     */
     public void showGoodbye() {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
+    /**
+     * Reads a line of input from the user.
+     *
+     * @return The user's input as a String.
+     */
     public String readCommand() {
         return scanner.nextLine();
     }
 
+    /**
+     * Displays a horizontal line for better readability.
+     */
     public void showLine() {
         System.out.println(LINE);
     }
 
+    /**
+     * Displays an error message to the user.
+     *
+     * @param message The error message to be displayed.
+     */
     public void showError(String message) {
         System.out.println(message);
     }
 
+    /**
+     * Displays the list of tasks to the user.
+     *
+     * @param tasks The TaskList containing the tasks to be displayed.
+     */
     public void showList(TaskList tasks) {
         if (tasks.isEmpty()) {
             System.out.println("Your task list is empty.");
@@ -42,23 +70,45 @@ public class Ui {
         }
     }
 
+    /**
+     * Displays a message indicating that a task has been added.
+     *
+     * @param task The task that was added.
+     * @param size The new size of the task list.
+     */
     public void showTaskAdded(Task task, int size) {
         System.out.println("Got it. I've added this task:");
         System.out.println("  " + task);
         System.out.println("Now you have " + size + " tasks in the list.");
     }
 
+    /**
+     * Displays a message indicating that a task has been deleted.
+     *
+     * @param task The task that was deleted.
+     * @param size The new size of the task list.
+     */
     public void showTaskDeleted(Task task, int size) {
         System.out.println("Noted. I've removed this task:");
         System.out.println("  " + task);
         System.out.println("Now you have " + size + " tasks in the list.");
     }
 
+    /**
+     * Displays a message indicating that a task has been marked as done.
+     *
+     * @param task The task that was marked as done.
+     */
     public void showTaskMarked(Task task) {
         System.out.println("Nice! I've marked this task as done:");
         System.out.println("  " + task);
     }
 
+    /**
+     * Displays a message indicating that a task has been unmarked as not done.
+     *
+     * @param task The task that was unmarked.
+     */
     public void showTaskUnmarked(Task task) {
         System.out.println("OK, I've marked this task as not done yet:");
         System.out.println("  " + task);
